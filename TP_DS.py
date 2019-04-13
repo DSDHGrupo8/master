d48048f759ef9fbcc4f3beeb32f209c5f817d37f
DSDHGrupo8 <49293490+DSDHGrupo8@users.noreply.github.com>
2019-04-12 21:31:46 -0300

    Add files via upload

---
+++ TP_DS.py
@@ -0,0 +1,130 @@
+!pip install -U -q PyDrive
+!pip install --upgrade scikit-learn
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from statistics import mode 
+from matplotlib import cm as cm
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
+
+import sklearn as sk
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.svm import SVR
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import svm
+from datetime import datetime
+
+class tp1_DS:
+  def __init__(self):
+    print("Version de SciKit:" ,sk.__version__)
+    # Authenticate and create the PyDrive client.
+    auth.authenticate_user()
+    gauth = GoogleAuth()
+    gauth.credentials = GoogleCredentials.get_application_default()
+    drive = GoogleDrive(gauth)
+    link = 'https://drive.google.com/open?id=10yw7Aopax6xH572LiDe_pa-oHaBNOuag' # The shareable link
+    #link = 'https://drive.google.com/open?id=1GXhb9LJJshv_gFdiMS6PujIG8SlhaZqv' # The shareable link
+    fluff, id = link.split('=')
+    downloaded = drive.CreateFile({'id':id}) 
+    downloaded.GetContentFile('Properati_limpio.csv')  
+    self.df=pd.read_csv("Properati_limpio.csv",encoding = 'utf8')
+    
+    #Convertir campos categoricos a valores enteros
+    dfStates=pd.DataFrame(self.df["state_name"].unique(),columns=['name'])
+    dfStates["ID"]=list(range(len(dfStates)))
+    dfPlaces=pd.DataFrame(self.df["place_name"].unique(),columns=['name'])
+    dfPlaces["ID"]=list(range(len(dfPlaces)))
+    dfPropertyTypes=pd.DataFrame(self.df["property_type"].unique(),columns=['name'])
+    dfPropertyTypes["ID"]=list(range(len(dfPropertyTypes)))
+    
+    #print(dfStates.head(5))
+    #print(dfPlaces.head(5))
+    #print(dfPropertyTypes.head(5))
+    
+    startTime=datetime.utcnow()
+    
+    print("Cant. registros del dataset:", len(self.df))
+    
+    for index, row in self.df.iterrows():
+      row.state_code=dfStates[dfStates.name==row.state_name].ID
+      row.place_code=dfPlaces[dfPlaces.name==row.place_name].ID
+      row.property_type_code=dfPropertyTypes[dfPropertyTypes.name==row.property_type].ID
+      
+    endTime=datetime.utcnow()
+    
+    print("Tiempo de ejecución:" , (endTime-startTime).total_seconds(), " segundos")
+    
+    #print(self.df["state_code"])
+    print("All done!")
+    
+  def analizarRegistrosRotos(self):
+    
+    self.df=self.df.fillna(0)
+    print("Cantidad registros:",len(self.df))
+    dfNulos0=self.df[(self.df.surface_total_in_m2==0) & (self.df.price_aprox_usd==0) & (self.df.price_usd_per_m2==0)] 
+    dfNulos1=self.df[(self.df.surface_total_in_m2==0) & (self.df.price_aprox_usd==0)] 
+    dfNulos2=self.df[(self.df.price_usd_per_m2==0) & (self.df.surface_total_in_m2==0)] 
+    dfNulos3=self.df[(self.df.price_usd_per_m2==0) & (self.df.price_aprox_usd==0)]
+    
+    dfNulos4=self.df[(self.df.price_usd_per_m2==0)]
+    dfNulos5=self.df[(self.df.price_aprox_usd==0)]
+    dfNulos6=self.df[(self.df.surface_total_in_m2==0)]
+    
+    print("Registros rotos tipo 0 (irrecuperable):" ,np.round(len(dfNulos0)/len(self.df)*100,2) , "%")
+    
+    print("Registros rotos tipo 1 (sup total. m2./ precio aprox. usd.):" ,np.round(len(dfNulos1)/len(self.df)*100,2), "%")
+    print("Registros rotos tipo 2 (sup. total m2./ precio por m2):" ,np.round(len(dfNulos2)/len(self.df)*100,2), "%")
+    print("Registros rotos tipo 3 (precio por m2 / precio en usd):" ,np.round(len(dfNulos3)/len(self.df)*100,2), "%")
+
+    print("Registros rotos tipo 4 (precio por m2):" ,np.round(len(dfNulos4)/len(self.df)*100,2), "%")
+    print("Registros rotos tipo 5 (precio aprox usd):" ,np.round(len(dfNulos5)/len(self.df)*100,2), "%")
+    print("Registros rotos tipo 6 (superficie total):" ,np.round(len(dfNulos6)/len(self.df)*100,2), "%")
+    
+  def predecir(self):
+      #prepare dataset  
+      #....   
+      #spilt dataset
+      campos_entrada=['state_code','place_code','property_type_code','surface_total_in_m2','precio_m2_usd']
+      print("State Name Codes:" ,self.df.state_name.codes())
+      
+      
+      Xtrn, Xtest, Ytrn, Ytest = train_test_split(self.df[campos_entrada], self.df['price_aprox_usd'],
+                                                  test_size=0.2)
+      
+      models = [LinearRegression(),
+                RandomForestRegressor(n_estimators=100, max_features='sqrt'),
+                KNeighborsRegressor(n_neighbors=6),
+                SVR(kernel='linear'),
+                LogisticRegression()
+                ]
+
+      TestModels = pd.DataFrame()
+      tmp = {}
+ 
+      for model in models:
+          # get model name
+          m = str(model)
+          tmp['Model'] = m[:m.index('(')]
+          # fit model on training dataset
+          model.fit(Xtrn, Ytrn['price_aprox_usd'])
+          # predict prices for test dataset and calculate r^2
+          tmp['R2_Price'] = r2_score(Ytest['price_aprox_usd'], model.predict(Xtest))
+          # write obtained data
+          TestModels = TestModels.append([tmp])
+
+      TestModels.set_index('Model', inplace=True)
+
+      fig, axes = plt.subplots(ncols=1, figsize=(10, 4))
+      TestModels.R2_Price.plot(ax=axes, kind='bar', title='R2_Price')
+      plt.show()
+
+x=tp1_DS()
+#x.analizarRegistrosRotos()
+x.predecir()
---
+++ TP_ETL.py
@@ -0,0 +1,207 @@
+!pip install -U -q PyDrive
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from statistics import mode 
+from matplotlib import cm as cm
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
+
+from sklearn import svm
+
+
+
+
+pd.set_option('display.expand_frame_repr', False)
+pd.options.display.float_format = '{:.2f}'.format
+
+class tp1_ETL:
+  
+     
+  
+    def conteo_por_grupos(self):
+        totalregs=len(self.df)
+        self.df['COUNTER'] =1       #initially, set that counter to 1.
+        #g=self.df.groupby(['state_name','place_name'])['COUNTER'].size();
+        g=self.df.groupby(['state_name','place_name'])['COUNTER'].size()/totalregs * 100;
+        g=g.sort_values(ascending=False)
+        print(g)
+  
+    def correlation_matrix(self):
+        
+            
+        #plt.rcParams.update({'font.size': 12})
+        fig = plt.figure()
+        fig.set_figheight(10)
+        fig.set_figwidth(40)
+        ax1 = fig.add_subplot(111)
+        cmap = cm.get_cmap('jet', 20)
+        cax = ax1.imshow(self.df.corr(), interpolation="nearest", cmap=cmap)
+        ax1.grid(True)
+        plt.title('Correlación entre variables')
+        labels=self.df.columns
+        ax1.set_xticklabels(labels,fontsize=12)
+        ax1.set_yticklabels(labels,fontsize=12)
+        # Add colorbar, make sure to specify tick locations to match desired ticklabels
+        fig.colorbar(cax, ticks=[0,0.25,0.50,0.75,1])
+        plt.show()
+    
+    def mostrarHistogramas(self):
+        
+        precioSerie=self.df['price_aprox_usd']
+        
+        print("******************PRECIO**************************************")
+        print("Promedio de Precio:" + str(np.round(np.mean(precioSerie),2)))
+        print("Mediana del Precio:" + str(np.round(np.median(precioSerie),2)))
+        print("Moda:" + str(mode(precioSerie)))
+        
+        plt.hist(precioSerie,bins=50)
+        plt.xlabel("Precio en USD - propiedades")
+        plt.ylabel("Frecuencia")
+        plt.show()
+        
+        supSerie=self.df['surface_total_in_m2']
+        
+        print("******************SUPERFICIE**************************************")
+        print("Promedio de superficie:" + str(np.round(np.mean(supSerie),2)))
+        print("Mediana de superficie:" + str(np.round(np.median(supSerie),2)))
+        print("Moda:" + str(mode(supSerie)))
+        
+        
+        print("Cantidad de registros con price mayor > 0 sin nulos:" ,str(len(precioSerie)))
+        
+        plt.hist(supSerie,bins=50)
+        plt.xlabel("Superficie m2 - propiedades")
+        plt.ylabel("Frecuencia")
+        plt.show()
+
+    def __init__(self):
+      # Authenticate and create the PyDrive client.
+        auth.authenticate_user()
+        gauth = GoogleAuth()
+        gauth.credentials = GoogleCredentials.get_application_default()
+        drive = GoogleDrive(gauth)
+        linkDS = 'https://drive.google.com/open?id=1GXhb9LJJshv_gFdiMS6PujIG8SlhaZqv' # The shareable link
+        fluff, id = linkDS.split('=')
+        downloaded = drive.CreateFile({'id':id}) 
+        downloaded.GetContentFile('properatti.csv')
+        
+        #https://drive.google.com/open?id=1HXMeE6Endm3g4c3JeiYIggdNcoQk6IqR -> DataSet con Lookup table Precio X m2
+        link_lookupPxM2="https://drive.google.com/open?id=1HXMeE6Endm3g4c3JeiYIggdNcoQk6IqR"
+        fluff, id = link_lookupPxM2.split('=')
+        downloaded = drive.CreateFile({'id':id}) 
+        downloaded.GetContentFile('precioxm2_pais.csv')
+        
+        self.df=pd.read_csv("properatti.csv", encoding = 'utf8')
+        self.df_m2=pd.read_csv("precioxm2_pais.csv", encoding = 'utf8')
+        print("DataSet registros:", len(self.df))
+        print("DataSet Lookup Precio x m2:", len(self.df_m2))
+        
+        #FIJAR SCOPE EN CABA
+        self.df=self.df.loc[self.df['state_name'] == 'Capital Federal']
+            
+        #CORRECCION DE PRECIOS y MONEDA
+        df1 = self.df[self.df['price'].isnull()]
+        aux = df1['title'].str.extract(r'(U?u?\$[SDsd]?)\s?(\d+)\.?(\d*)\.?(\d*)')
+        aux=aux.dropna()
+        aux[0]=aux[0].str.replace('^\$$', 'ARS', regex=True)
+        aux[0]=aux[0].str.replace('^[^A].*$', 'USD', regex=True)
+        aux['currency']=aux[0]
+        aux['price']=aux[1]+aux[2]+aux[3]
+        aux['price']=aux['price'].astype('float64')
+        aux=aux[['currency','price']]
+        self.df.loc[self.df['price'].isnull(),'price'] = aux['price']
+        self.df.loc[self.df['currency'].isnull(),'currency'] = aux['currency']
+        df1 = self.df[self.df['price'].isnull()]
+        aux = df1['description'].str.extract(r'(U?u?\$[SDsd]?)\s?(\d+)\.?(\d*)\.?(\d*)')
+        aux=aux.dropna()
+        aux[0]=aux[0].str.replace('^\$$', 'ARS', regex=True)
+        aux[0]=aux[0].str.replace('^[^A].*$', 'USD', regex=True)
+        aux['currency']=aux[0]
+        aux['price']=aux[1]+aux[2]+aux[3]
+        aux['price']=aux['price'].astype('float64')
+        aux=aux[['currency','price']]
+        self.df.loc[self.df['price'].isnull(),'price'] = aux['price']
+        self.df.loc[self.df['currency'].isnull(),'currency'] = aux['currency']
+        
+        #CORRECCION DE M2
+        
+        
+        
+        #JOINEAR CON TABLA LOOKUP
+        #values = {'surface_total_in_m2': 0}
+        #self.df=self.df.fillna(value=values)
+        #aux_caba=self.df.merge(df_m2, how='left', on='place_name')
+        #values = {'m2_Desde': 0, 'm2_Hasta': 999999}
+        #aux_caba=aux_caba.fillna(value=values)
+        #aux_caba=aux_caba[(aux_caba['surface_total_in_m2']>=aux_caba['m2_Desde']) & (aux_caba['surface_total_in_m2']<=aux_caba['m2_Hasta'])]
+        #aux_caba=aux_caba.drop(['state_name_y', 'm2_Desde', 'm2_Hasta'], axis=1)
+        
+        
+        #DROPEAMOS VARIABLES NO INTERESANTES
+        #dropeo columnas que no son de interés
+        cols=['country_name', 'currency','price', 'price_aprox_local_currency','operation','lat','lon','properati_url','description','title','place_with_parent_names','image_thumbnail','floor','rooms','geonames_id','price_usd_per_m2']
+        self.df.drop(cols, axis=1, inplace=True)
+        self.df.dropna(subset=['surface_total_in_m2','price_aprox_usd'],inplace=True)
+
+        self.df2=self.df.dropna()
+        #print(self.df.columns)
+        
+        #ARREGLAR DATOS CORREGIBLES
+        #Arreglar precio x m2 en dólares
+        self.df['price_aprox_usd']=np.round(self.df['price_aprox_usd'],0).fillna(0).astype(np.int64)
+        self.df['surface_total_in_m2']=np.round(self.df['surface_total_in_m2'],0).fillna(0).astype(np.int64)
+        self.df['precio_m2_usd']=np.round(self.df['price_aprox_usd'] / self.df['surface_total_in_m2'],0)
+        
+        #auxdf=self.df.query('precio_m2_usd>10000').filter(items=['precio_m2_usd','price_aprox_usd','surface_total_in_m2'])
+        #print(auxdf)
+        #print(self.df['precio_m2_usd'])
+        
+        #Arreglar latitud y longitud por medio de la columna lat/lon
+        latlongdf=self.df['lat-lon'].str.split(",",expand=True)
+        self.df['lat']=latlongdf.loc[:,0]
+        self.df['lon']=latlongdf.loc[:,1]
+        self.df.drop('lat-lon',axis=1,inplace=True)
+        self.df.drop('price_per_m2',axis=1,inplace=True)
+        
+        
+        qryFiltro='(price_aprox_usd >= 10000 and price_aprox_usd <= 1000000) '
+        qryFiltro+='and (surface_total_in_m2 >= 20 and surface_total_in_m2 <= 1000) '
+        qryFiltro+='and (surface_total_in_m2 >= surface_covered_in_m2)'
+        
+        #print("Query de filtrado:" , qryFiltro)
+        self.df=self.df.query(qryFiltro)
+        
+        #Falta que grabe al Google Drive de nuevo - esta parte no funca 
+        # pero el dataframe está limpio y filtrado
+        self.df.to_csv("Properati_fixed.csv",encoding='utf-8')
+        uploaded = drive.CreateFile({'Properati_fixed': 'Properati_fixed.csv'})
+        uploaded.SetContentFile("Properati_fixed.csv")
+        uploaded.Upload()
+        
+        print("All done!")
+        
+
+    def correrAnalisis(self):
+        
+        
+        print("Conteo total de registros sin nulos en price y surface_total:" , str(len(self.df3)))
+        
+        #print(precioSerie)
+        
+        
+        print("**********************DF2*******************************")
+        print(self.df2.describe())
+        print("*****************************************************")
+        
+        
+        print("**********************DF3*******************************")
+        print(self.df3.describe())
+        print("*****************************************************")
+
+x=tp1_ETL()
+#x.correlation_matrix()
+x.conteo_por_grupos()
