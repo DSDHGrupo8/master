6f43faf0b265bd0ffac03ceabf362eba46f57e94
DSDHGrupo8 <43507059+neonknightz666@users.noreply.github.com>
2019-04-13 17:35:41 -0300

    asdasd

--- TP_ExtraerDatosCABA.py
+++ TP_ExtraerDatosCABA.py
@@ -12,4 +12,4 @@ df2=df.query("state_name=='Capital Federal'")
 cols=['country_name', 'currency', 'price_aprox_local_currency','operation','lat','lon','properati_url','description','title','place_with_parent_names','image_thumbnail','floor','rooms','geonames_id','price_usd_per_m2']
 df2.drop(cols, axis=1, inplace=True)
 df2.dropna(subset=['price', 'surface_total_in_m2'],inplace=True)
-df2.to_csv("Properati_CABA.csv")
+df2.to_csv("Properati_CABA.csv",encoding = 'utf8')
--- TP_prepararDS.py
+++ TP_prepararDS.py
@@ -11,36 +11,44 @@ import math
 
 df=pd.read_csv("Properati_CABA.csv",encoding = 'utf8')
 #Convertir campos categoricos a valores enteros
-dfStates=pd.DataFrame(df["state_name"].unique(),columns=['name'])
-dfStates["ID"]=list(range(len(dfStates)))
+#dfStates=pd.DataFrame(df["state_name"].unique(),columns=['name'])
+#dfStates["ID"]=list(range(len(dfStates)))
 dfPlaces=pd.DataFrame(df["place_name"].unique(),columns=['name'])
 dfPlaces["ID"]=list(range(len(dfPlaces)))
 dfPropertyTypes=pd.DataFrame(df["property_type"].unique(),columns=['name'])
 dfPropertyTypes["ID"]=list(range(len(dfPropertyTypes)))
 
-#print(dfStates.head(5))
-#print(dfPlaces.head(5))
-#print(dfPropertyTypes.head(5))
+#print(dfStates)
+#print(dfPlaces)
+#print(dfPropertyTypes)
 
 startTime=datetime.utcnow()
 
 print("Cant. registros del dataset:", len(df))
 
 auxval=0
+df["state_code"]=0
+df["place_code"]=0
+df["property_type_code"]=0
 
 for index, row in df.iterrows():
   if (math.fmod(index,1000)==0):print("Processing row:", index)
-  auxval=dfStates[dfStates.name==row.state_name].ID
-  df.set_value(index,"state_code",auxval)
-  auxval=dfPlaces[dfPlaces.name==row.place_name].ID
-  df.set_value(index,"place_code",auxval)
-  auxval=dfPropertyTypes[dfPropertyTypes.name==row.property_type].ID
-  df.set_value(index,"property_type_code",auxval)
-  #print("row.property_type_code:",row.property_type_code)
-
+  df.at[index,"state_code"]=0
+  
+  if not (row.place_name is ''):
+    auxval=dfPlaces.query("name=='" + row.place_name + "'").ID
+    df.at[index,"place_code"]=auxval
+      
+  if not (row.property_type is ''):
+    auxval=dfPropertyTypes.query("name=='" + row.property_type + "'").ID
+    #print("property_type", row.property_type)
+    #print("auxval found:", auxval)
+    df.at[index,"property_type_code"]=auxval
+    #print("row.property_type:",row.property_type)
+  
 endTime=datetime.utcnow()
 
 print("Time elapsed:", (endTime - startTime).total_seconds())
 
 print(df.info())
-df.to_csv("Properati_CABA_DS.csv")
+df.to_csv("Properati_CABA_DS.csv") 
