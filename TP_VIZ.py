6a4656627b0616b80a9d4a138e9e1d61ac507de4
DSDHGrupo8 <43507059+neonknightz666@users.noreply.github.com>
2019-04-25 20:29:10 -0300

    Recorté código que está en el TP_ETL.py

--- TP_VIZ.py
+++ TP_VIZ.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from statistics import mode 
 from matplotlib import cm as cm
-from sklearn import svm
+#from sklearn import svm
 import seaborn as sns
 
 pd.set_option('display.expand_frame_repr', False)
@@ -20,11 +20,11 @@ class tp1_VIZ:
         print(g)
   
     def correlation_matrix(self):
-                    
+        self.df = pd.read_csv('Properati_CABA_DS_fixed.csv')
         #plt.rcParams.update({'font.size': 12})
         fig = plt.figure()
         fig.set_figheight(10)
-        fig.set_figwidth(40)
+        fig.set_figwidth(50)
         ax1 = fig.add_subplot(111)
         cmap = cm.get_cmap('jet', 20)
         cax = ax1.imshow(self.df.corr(), interpolation="nearest", cmap=cmap)
@@ -66,89 +66,6 @@ class tp1_VIZ:
         plt.ylabel("Frecuencia")
         plt.show()
 
-    def __init__(self):
-        self.df=pd.read_csv("properatti.csv", encoding = 'utf8')
-        self.df_m2=pd.read_csv("precioxm2_pais.csv", encoding = 'utf8')
-        print("DataSet registros:", len(self.df))
-        print("DataSet Lookup Precio x m2:", len(self.df_m2))
-        
-        #FIJAR SCOPE EN CABA
-        self.df=self.df.loc[self.df['state_name'] == 'Capital Federal']
-            
-        #CORRECCION DE PRECIOS y MONEDA
-        df1 = self.df[self.df['price'].isnull()]
-        aux = df1['title'].str.extract(r'(U?u?\$[SDsd]?)\s?(\d+)\.?(\d*)\.?(\d*)')
-        aux=aux.dropna()
-        aux[0]=aux[0].str.replace('^\$$', 'ARS', regex=True)
-        aux[0]=aux[0].str.replace('^[^A].*$', 'USD', regex=True)
-        aux['currency']=aux[0]
-        aux['price']=aux[1]+aux[2]+aux[3]
-        aux['price']=aux['price'].astype('float64')
-        aux=aux[['currency','price']]
-        self.df.loc[self.df['price'].isnull(),'price'] = aux['price']
-        self.df.loc[self.df['currency'].isnull(),'currency'] = aux['currency']
-        df1 = self.df[self.df['price'].isnull()]
-        aux = df1['description'].str.extract(r'(U?u?\$[SDsd]?)\s?(\d+)\.?(\d*)\.?(\d*)')
-        aux=aux.dropna()
-        aux[0]=aux[0].str.replace('^\$$', 'ARS', regex=True)
-        aux[0]=aux[0].str.replace('^[^A].*$', 'USD', regex=True)
-        aux['currency']=aux[0]
-        aux['price']=aux[1]+aux[2]+aux[3]
-        aux['price']=aux['price'].astype('float64')
-        aux=aux[['currency','price']]
-        self.df.loc[self.df['price'].isnull(),'price'] = aux['price']
-        self.df.loc[self.df['currency'].isnull(),'currency'] = aux['currency']
-        
-        #CORRECCION DE M2
-             
-        #JOINEAR CON TABLA LOOKUP
-        #values = {'surface_total_in_m2': 0}
-        #self.df=self.df.fillna(value=values)
-        #aux_caba=self.df.merge(df_m2, how='left', on='place_name')
-        #values = {'m2_Desde': 0, 'm2_Hasta': 999999}
-        #aux_caba=aux_caba.fillna(value=values)
-        #aux_caba=aux_caba[(aux_caba['surface_total_in_m2']>=aux_caba['m2_Desde']) & (aux_caba['surface_total_in_m2']<=aux_caba['m2_Hasta'])]
-        #aux_caba=aux_caba.drop(['state_name_y', 'm2_Desde', 'm2_Hasta'], axis=1)
-        
-        
-        #DROPEAMOS VARIABLES NO INTERESANTES
-        #dropeo columnas que no son de interés
-        cols=['country_name', 'currency','price', 'price_aprox_local_currency','operation','lat','lon','properati_url', 'expenses','place_with_parent_names','image_thumbnail','floor','rooms','geonames_id','price_usd_per_m2']
-        self.df.drop(cols, axis=1, inplace=True)
-        self.df.dropna(subset=['surface_total_in_m2','price_aprox_usd'],inplace=True)
-
-        self.df2=self.df.dropna()
-        #print(self.df.columns)
-        
-        #ARREGLAR DATOS CORREGIBLES
-        #Arreglar precio x m2 en dólares
-        self.df['price_aprox_usd']=np.round(self.df['price_aprox_usd'],0).fillna(0).astype(np.int64)
-        self.df['surface_total_in_m2']=np.round(self.df['surface_total_in_m2'],0).fillna(0).astype(np.int64)
-        self.df['precio_m2_usd']=np.round(self.df['price_aprox_usd'] / self.df['surface_total_in_m2'],0)
-        
-        #auxdf=self.df.query('precio_m2_usd>10000').filter(items=['precio_m2_usd','price_aprox_usd','surface_total_in_m2'])
-        #print(auxdf)
-        #print(self.df['precio_m2_usd'])
-        
-        #Arreglar latitud y longitud por medio de la columna lat/lon
-        latlongdf=self.df['lat-lon'].str.split(",",expand=True)
-        self.df['lat']=latlongdf.loc[:,0]
-        self.df['lon']=latlongdf.loc[:,1]
-        self.df.drop('lat-lon',axis=1,inplace=True)
-        self.df.drop('price_per_m2',axis=1,inplace=True)
-        
-        
-        qryFiltro='(price_aprox_usd >= 10000 and price_aprox_usd <= 1000000) '
-        qryFiltro+='and (surface_total_in_m2 >= 20 and surface_total_in_m2 <= 1000) '
-        qryFiltro+='and (surface_total_in_m2 >= surface_covered_in_m2)'
-        
-        #print("Query de filtrado:" , qryFiltro)
-        self.df=self.df.query(qryFiltro)
-        
-        self.df.to_csv("Properati_fixed.csv",encoding='utf-8')
-                
-        print("All done!")
-        
 
     def correrAnalisis(self):
         
@@ -171,7 +88,7 @@ x=tp1_VIZ()
 #x.correlation_matrix()
 #x.conteo_por_grupos()
 
-df2 = pd.read_csv('Properati_fixed.csv')
+df2 = pd.read_csv('Properati_CABA_DS_fixed.csv')
 cols = ['Unnamed: 0', 'Unnamed: 0.1']
 df2.drop(cols, axis=1, inplace=True)
 df2.loc[:,'property_type'].value_counts().to_frame()
@@ -182,9 +99,9 @@ df2.loc[:,'property_type'].value_counts().to_frame()
 df2['place_name'] = df_test=df2.loc[:,'place_name'].replace(['Palermo Soho','Palermo Hollywood','Palermo Chico','Palermo Viejo'], 'Palermo')
 df2['property_type'] = df_test=df2.loc[:,'property_type'].replace('PH', 'house')
 #Valores categóricos por columnas
-print(df2.loc[:,'state_name'].unique())
-print(df2.loc[:,'place_name'].unique())
-print(df2.loc[:,'property_type'].unique())
+#print(df2.loc[:,'state_name'].unique())
+#print(df2.loc[:,'place_name'].unique())
+#print(df2.loc[:,'property_type'].unique())
 #Duplicados de description  
 df2.duplicated('description').value_counts().to_frame()
 
@@ -192,7 +109,7 @@ df2.duplicated('description').value_counts().to_frame()
 df2.duplicated(subset=['lat','lon','description']).value_counts().to_frame()
 
 #¿Qué pasaba en el original?
-df_original = pd.read_csv('properatti.csv')
+df_original = pd.read_csv('C:\\Users\\Public\\properati.csv')
 df_original.duplicated(subset=['lat','lon','description']).value_counts().to_frame()
 
 #Crear un DF nuevo sin duplicados
@@ -202,7 +119,8 @@ df3.count()
 df2.iloc[:,:9].describe()
 df3.iloc[:,:9].describe()
 
-sns.lmplot(x="surface_total_in_m2", y="price_aprox_usd", data = df3, hue = 'place_name', height=10, fit_reg=False);
+#sns.lmplot(x="surface_total_in_m2", y="price_aprox_usd", data = df3, hue = 'place_name', height=10, fit_reg=False);
+sns.lmplot(x="surface_total_in_m2", y="price_aprox_usd", data = df3, hue = 'place_name',fit_reg=False);
 plt.savefig('grafico')
 #Distribución de precio aproximado
 sns.distplot(df3.loc[:,'price_aprox_usd'], kde=False);
