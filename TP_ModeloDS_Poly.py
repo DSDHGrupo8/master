98ade8cbfd726337d9f57282332fc0aefe0e04a9
DSDHGrupo8 <49293490+DSDHGrupo8@users.noreply.github.com>
2019-05-27 21:58:35 -0300

    Update TP_ModeloDS_Poly.py

--- TP_ModeloDS_Poly.py
+++ TP_ModeloDS_Poly.py
@@ -30,14 +30,31 @@ y_test=df_test["precio_m2_usd"]
 #print("X:",X)
 #print("y:",y)
 
-polynomial_features= PolynomialFeatures(degree=2)
-x_poly = polynomial_features.fit_transform(x)
+degree=2
+poly_features = PolynomialFeatures(degree=degree)
 
-model = LinearRegression()
-model.fit(x_poly, y)
-y_poly_pred = model.predict(x_poly)
+# transforms the existing features to higher degree features.
+X_train_poly = poly_features.fit_transform(X_train)
 
-rmse = np.sqrt(mean_squared_error(y,y_poly_pred))
-r2 = r2_score(y,y_poly_pred)
-print("Poly rmse:",rmse)
-print("Poly r2:",r2)
+# fit the transformed features to Linear Regression
+poly_model = LinearRegression()
+poly_model.fit(X_train_poly, Y_train)
+
+# predicting on training data-set
+y_train_predicted = poly_model.predict(X_train_poly)
+
+# predicting on test data-set
+y_test_predict = poly_model.predict(poly_features.fit_transform(X_test))
+
+# evaluating the model on training dataset
+rmse_train = np.sqrt(mean_squared_error(Y_train, y_train_predicted))
+r2_train = r2_score(Y_train, y_train_predicted)
+
+# evaluating the model on test dataset
+rmse_test = np.sqrt(mean_squared_error(Y_test, y_test_predict))
+r2_test = r2_score(Y_test, y_test_predict)
+
+print("The model performance for the training set")
+print("-------------------------------------------")
+print("RMSE of training set is {}".format(rmse_train))
+print("R2 score of training set is {}".format(r2_train))
