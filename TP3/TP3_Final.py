cf6432e860b3b8c2a6ad4dbaf77ad450f7ee740c
DSDHGrupo8 <49293490+DSDHGrupo8@users.noreply.github.com>
2019-06-24 20:47:55 -0300

    Agregado Cross Validation

--- TP3/TP3_Final.py
+++ TP3/TP3_Final.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 22 13:36:44 2019
-
-@author: Adrian
-"""
-
 import math
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -20,8 +13,10 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split,cross_val_predict,cross_val_score
+from sklearn import metrics
 
-df = pd.read_csv('datasets/train.csv')
+df = pd.read_csv('train.csv')
 
 X = df.drop(['price_range'],axis=1)
 y = df['price_range']
@@ -40,13 +35,16 @@ X_test_std_df = pd.DataFrame(X_test_std, index=X_test.index, columns=X_test.colu
 
 modelo_NB = MultinomialNB()
 #print("X_train_std:", X_train_std)
+modelo_NB.fit(X_train_std, y_train)
+predicciones=cross_val_predict(modelo_NB, X_train_std, y_train, cv=6)
 
-modelo_NB.fit(X_train_std_df, y_train)
+scores = cross_val_score(modelo_NB, X_train_std, y_train, cv=6)
+print("Mejor puntaje NB:",round(max(scores),4))
 
-prediccion = modelo_NB.predict(X_test_std)
+#prediccion = modelo_NB.predict(X_test_std)
 
 # Primero calculamos el accuracy general del modelo
-print("accuracy NB:" , accuracy_score(y_test, prediccion))
+#print("accuracy NB:" , accuracy_score(y_test, prediccion))
 
 #estandarizar
 scaler2 = StandardScaler().fit(X_train)
@@ -57,18 +55,12 @@ X_test_std = scaler2.transform(X_test)
 X_train_std_df = pd.DataFrame(X_train_std, index=X_train.index, columns=X_train.columns)
 X_test_std_df = pd.DataFrame(X_test_std, index=X_test.index, columns=X_test.columns) 
 
-
-mat = confusion_matrix(y_test, prediccion)
+mat = confusion_matrix(y_train, predicciones)
 
 sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False)
 #xticklabels=['negativo','positivo'], yticklabels=['negativo','positivo']
 plt.xlabel('Etiquetas verdaderas')
-plt.ylabel('Etiquetas predichas');
-
-##elijo componentes principales
-#pca = PCA(n_components = 3) 
-#X_train_pca = pca.fit_transform(X_train_std_df)
-#X_test_pca = pca.transform(X_test_std_df)
+plt.ylabel('Etiquetas predichas')
 
 #modelos
 svm_lin = SVC(kernel='sigmoid',degree=3,C=10, gamma='auto')
@@ -79,24 +71,16 @@ svm_lin.fit(X_train_std_df, y_train)
 knn = KNeighborsClassifier()
 
 best_k = int(round(math.sqrt(len(X)),0))
-print("K selected:" , best_k)
+print("K selected for KNN:" , best_k)
 knn = KNeighborsClassifier(n_neighbors=best_k)
 
 knn.fit(X_train_std_df, y_train)
-#score= knn_gscv.score(X_test_std_df, y_test)
-score= knn.score(X_test_std_df, y_test)
-print("Score de KNN:", score)
+predicciones=cross_val_predict(knn, X_train_std, y_train, cv=6)
 
-#Applying grid search for optimal parameters and model after k-fold validation
+#score= knn.score(X_test_std_df, y_test)
+scores = cross_val_score(knn, X_train_std, y_train, cv=6)
+print("Mejor puntaje KNN (CV=6):",round(max(scores),4))
 
-#parameters = [{'C':[0.01,0.1,1,10,50,100,500,1000], 'kernel':['rbf'], 'gamma': [0.1,0.125,0.15,0.17,0.2]}]
-#grid_search = GridSearchCV(estimator=svm_rbf, param_grid=parameters, scoring ='accuracy',cv=10,n_jobs=-1)
-#grid_search = grid_search.fit(X_train_std,y_train)
-#
-#best_accuracy = grid_search.best_score_
-#opt_param = grid_search.best_params_
-#print("best accuracy:" , best_accuracy)
-#print("opt_param:" , opt_param)
 
 #predicciones
 #prediccion_svm_lin = svm_lin.predict(X_test_std_df)
@@ -106,15 +90,5 @@ print("Score de KNN:", score)
 #print(accuracy_score(y_test, prediccion_svm_lin))
 #print(accuracy_score(y_test, prediccion_svm_rbf))
 
-print("SVM-lin score:", svm_lin.score(X_test_std_df, y_test))
+print("SVM-sigmoid score:", svm_lin.score(X_test_std_df, y_test))
 print("SVM-rbf score:", svm_rbf.score(X_test_std_df, y_test))
-
-
-
-
-
-
-
-
-
-
