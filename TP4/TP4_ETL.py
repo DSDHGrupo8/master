86dbcd436b5d9f135c18491240cf069d710356f7
DSDHGrupo8 <43507059+neonknightz666@users.noreply.github.com>
2019-07-04 19:19:15 -0300

    Eliminados TP4_EXPL1.py y TP4_PIPE.py Arreglado el código de TP4_ETL.py Arreglado el código de TP4/TP4.py

--- TP4/TP4_ETL.py
+++ TP4/TP4_ETL.py
@@ -1,175 +1,62 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 29 12:24:45 2019
-
-@author: Adrian
-"""
-import numpy as np
 import pandas as pd
-from sklearn.svm import SVC
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import Normalizer
-from sklearn.model_selection import train_test_split,GridSearchCV
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.pipeline import Pipeline
 
-class colPreProcessor(BaseEstimator, TransformerMixin):
-    def __init__(self, key):
-        self.key = key
+ 
 
-    def fit(self, X, y=None):
-        return self
+print("pandas:" , pd.__version__)
 
-    def transform(self, X):
-        fields=["ProductName","EngineVersion","AvSigVersion",
-        "AVProductStatesIdentifier","AVProductsInstalled","AVProductsEnabled","HasTpm","CountryIdentifier",
-        "CityIdentifier","Platform","Processor","OsVer","OsPlatformSubRelease","IsProtected","SMode",
-        "SmartScreen","Firewall","Census_DeviceFamily","RtpStateBitfield","IsSxsPassiveMode",
-        "Census_OSArchitecture", "Census_OSWUAutoUpdateOptionsName","Census_IsPortableOperatingSystem",
-        "Census_GenuineStateName","Census_IsSecureBootEnabled","UacLuaenable"]
+fields=["ProductName","EngineVersion","AvSigVersion",
 
-        #Recortamos el dataset a los fields necesarios
-        X=X[fields]
-        
-        
-        #Dummificar las columnas de texto
-        #Una vez dummificadas dropear las viejas columnas de texto
-        
-        #Dummificar ProductName
-        dummies_productName=pd.get_dummies(X['ProductName'],prefix='dummy_ProductName',drop_first=True)
-        X=pd.concat([X,dummies_productName],axis=1)
-       
-        X.drop("ProductName",axis=1,inplace=True)
+    "AVProductStatesIdentifier","AVProductsInstalled","AVProductsEnabled","HasTpm","CountryIdentifier",
 
-        #Dummificar OsVer
-        dummies_OsVer=pd.get_dummies(X['OsVer'],prefix='dummy_OsVer',drop_first=True)
-        X=pd.concat([X,dummies_OsVer],axis=1)
+    "CityIdentifier","Platform","Processor","OsVer","OsPlatformSubRelease","IsProtected","SMode",
 
-        X.drop("OsVer",axis=1,inplace=True)
+    "SmartScreen","Firewall","Census_DeviceFamily","RtpStateBitfield","IsSxsPassiveMode",
 
-        #Dummificar EngineVersion
-        dummies_EngineVersion=pd.get_dummies(X['EngineVersion'],prefix='dummy_EngineVersion',drop_first=True)
-        X=pd.concat([X,dummies_EngineVersion],axis=1)
+    "Census_OSArchitecture", "Census_OSWUAutoUpdateOptionsName","Census_IsPortableOperatingSystem",
 
-        X.drop("EngineVersion",axis=1,inplace=True)
+    "Census_GenuineStateName","Census_IsSecureBootEnabled","UacLuaenable","HasDetections"]
 
-        #Dummificar AvSigVersion
-        dummies_AvSigVersion=pd.get_dummies(X['AvSigVersion'],prefix='dummy_AvSigVersion',drop_first=True)
-        X=pd.concat([X,dummies_AvSigVersion],axis=1)
+ 
 
-        X.drop("AvSigVersion",axis=1,inplace=True)
+#dataconversions={'Census_IsSecureBootEnabled': str, 'SmartScreen':str,'Census_OSArchitecture':str}
 
-        #Dummificar Platform
-        dummies_Platform=pd.get_dummies(X['Platform'],prefix='dummy_Platform',drop_first=True)
-        X=pd.concat([X,dummies_Platform],axis=1)
+#df = pd.read_csv('c:\\TEST\\TP4\\train.csv',usecols=fields,nrows=5000,dtype=dataconversions)
 
-        X.drop("Platform",axis=1,inplace=True)
+df = pd.read_csv('c:\\TEST\\TP4\\train.csv',usecols=fields,nrows=50000)
 
-        #Dummificar Processor
-        dummies_Processor=pd.get_dummies(X['Processor'],prefix='dummy_Processor',drop_first=True)
-        X=pd.concat([X,dummies_Processor],axis=1)
+print("df.info()=" ,df.info())
 
-        X.drop("Processor",axis=1,inplace=True)
+#print("df.describe()=" ,df.describe())
 
-        #Dummificar OsPlatformSubRelease
-        dummies_OsPlatformSubRelease=pd.get_dummies(X['OsPlatformSubRelease'],prefix='dummy_OsPlatformSubRelease',drop_first=True)
-        X=pd.concat([X,dummies_OsPlatformSubRelease],axis=1)
+print("len(df) before", len(df))
 
-        X.drop("OsPlatformSubRelease",axis=1,inplace=True)
+df=df[pd.notnull(df["SmartScreen"])]
 
-        #Dummificar SmartScreen
-        dummies_SmartScreen=pd.get_dummies(X['SmartScreen'],prefix='dummy_SmartScreen',drop_first=True)
-        X=pd.concat([X,dummies_SmartScreen],axis=1)
+minidf=df[df.SmartScreen.str.contains("&#x0")]
 
-        X.drop("SmartScreen",axis=1,inplace=True)
-      
-        #Dummificar Census_DeviceFamily
-        dummies_Census_DeviceFamily=pd.get_dummies(X['Census_DeviceFamily'],prefix='dummy_Census_DeviceFamily',drop_first=True)
-        X=pd.concat([X,dummies_Census_DeviceFamily],axis=1)
+print("len(minidf):", len(minidf))
 
-        X.drop("Census_DeviceFamily",axis=1,inplace=True)
+ 
 
-        #Dummificar Census_OSArchitecture
-        dummies_Census_OSArchitecture=pd.get_dummies(X['Census_OSArchitecture'],prefix='dummy_Census_OSArchitecture',drop_first=True)
-        X=pd.concat([X,dummies_Census_OSArchitecture],axis=1)
-
-        X.drop("Census_OSArchitecture",axis=1,inplace=True)
-
-        #Dummificar Census_OSWUAutoUpdateOptionsName
-        dummies_Census_OSWUAutoUpdateOptionsName=pd.get_dummies(X['Census_OSWUAutoUpdateOptionsName'],prefix='dummy_Census_OSWUAutoUpdateOptionsName',drop_first=True)
-        X=pd.concat([X,dummies_Census_OSWUAutoUpdateOptionsName],axis=1)
-
-        X.drop("Census_OSWUAutoUpdateOptionsName",axis=1,inplace=True)
-
-        #Dummificar Census_GenuineStateName
-        dummies_Census_GenuineStateName=pd.get_dummies(X['Census_GenuineStateName'],prefix='dummy_Census_GenuineStateName',drop_first=True)
-        X=pd.concat([X,dummies_Census_GenuineStateName],axis=1)
-
-        X.drop("Census_GenuineStateName",axis=1,inplace=True)
-
-        return X
+indexNames= df[df['SmartScreen'].str.contains("&#x0")].index
 
 
-   
-class myScaler(BaseEstimator, TransformerMixin):
-    def __init__(self, key):
-        self.key = key
+# Delete these row indexes from dataFrame
 
-    def fit(self, X, y=None):
-        return self
+df.drop(indexNames , inplace=True)
 
-    def transform(self, X):
-        scaler = Normalizer().fit(X)
-        scaler.transform(X)
-        return X
+ 
 
-class Imputer(BaseEstimator, TransformerMixin):
-    """
-    Transformer to select a single column from the data frame to perform additional transformations on
-    Use on text columns in the data
-    """
-    def __init__(self, key):
-        self.key = key
+df.query("Census_IsSecureBootEnabled in [0,1]",inplace=True)
 
-    def fit(self, X, y=None):
-        return self
+df.query("Census_OSArchitecture in ['amd64','arm64','x86','x64']",inplace=True)
 
-    def transform(self, X):
-        X.dropna(thresh=10,inplace=True)
-        #X.loc[X.SmartScreen.isnull(),"SmartScreen"]="Off"
-        X.loc[X.Firewall.isnull(),"IsProtected"]=0
-        X.loc[X.Firewall.isnull(),"Firewall"]=0
-        #Dropear rows donde UacLuenable no sea 0 o 1
-        X=X[X.UacLuaenable<=1]
-        #Dropear rows donde HasDetections no sea 0 o 1
-        #X=X[X.HasDetections<=1]
-        
+ 
 
-        X.loc[X.SMode.isnull(),"SMode"]=0
-        return X
+ 
 
-df=pd.read_csv("train_clean.csv",encoding="utf-8")
-df=df[df.HasDetections<=1]
-archs=['amd64','arm64','x86','x64']
-df=df[df.Census_OSArchitecture.isin(archs)]
-df=df[~df.Census_IsSecureBootEnabled=="IS_GENUINE"]
+print("len(df) after", len(df))
 
+df=df.fillna(0)
 
-Y=df["HasDetections"]
-X=df.drop("HasDetections",axis=1)
-del df
-
-X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size=0.2, random_state=30, stratify=Y)
-del X
-del Y
-
-steps = [("colPreProcessor",colPreProcessor("")),("imputer",Imputer("")),("scaler", myScaler("")), ("SVC",SVC())]
-
-pipeline = Pipeline(steps) # define the pipeline object.
-parametros = {'SVM__C':[0.001,0.1,10,100,10e5], 'SVM__gamma':[0.1,0.01]}
-grid = GridSearchCV(pipeline, param_grid=parametros, cv=5)
-
-pipeline.fit(X_train, y_train)
-preds = pipeline.predict(X_test)
-
-np.mean(preds == y_test)
+df.to_csv("c:\\TEST\\TP4\\train_clean.csv")
