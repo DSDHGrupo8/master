489a3ba9a65307ee7f3f0108c4fe014e40a51ad7
fedegonzalez <grupo8datascience@gmail.com>
2019-07-11 04:28:59 -0300

    ETL Fede

---
+++ TP4/TP4_ETL_FEDE.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import numpy as np
+import datetime
+	
+dtypes = {
+        'MachineIdentifier':                                    'category',
+        'ProductName':                                          'category',
+        'EngineVersion':                                        'category',
+        'AppVersion':                                           'category',
+        'AvSigVersion':                                         'category',
+        'IsBeta':                                               'int8',
+        'RtpStateBitfield':                                     'float16',
+        'IsSxsPassiveMode':                                     'int8',
+        'DefaultBrowsersIdentifier':                            'float16',
+        'AVProductStatesIdentifier':                            'float32',
+        'AVProductsInstalled':                                  'float16',
+        'AVProductsEnabled':                                    'float16',
+        'HasTpm':                                               'int8',
+        'CountryIdentifier':                                    'int16',
+        'CityIdentifier':                                       'float32',
+        'OrganizationIdentifier':                               'float16',
+        'GeoNameIdentifier':                                    'float16',
+        'LocaleEnglishNameIdentifier':                          'int8',
+        'Platform':                                             'category',
+        'Processor':                                            'category',
+        'OsVer':                                                'category',
+        'OsBuild':                                              'int16',
+        'OsSuite':                                              'int16',
+        'OsPlatformSubRelease':                                 'category',
+        'OsBuildLab':                                           'category',
+        'SkuEdition':                                           'category',
+        'IsProtected':                                          'float16',
+        'AutoSampleOptIn':                                      'int8',
+        'PuaMode':                                              'category',
+        'SMode':                                                'float16',
+        'IeVerIdentifier':                                      'float16',
+        'SmartScreen':                                          'category',
+        'Firewall':                                             'float16',
+        'UacLuaenable':                                         'float32',
+        'Census_MDC2FormFactor':                                'category',
+        'Census_DeviceFamily':                                  'category',
+        'Census_OEMNameIdentifier':                             'float16',
+        'Census_OEMModelIdentifier':                            'float32',
+        'Census_ProcessorCoreCount':                            'float16',
+        'Census_ProcessorManufacturerIdentifier':               'float16',
+        'Census_ProcessorModelIdentifier':                      'float16',
+        'Census_ProcessorClass':                                'category',
+        'Census_PrimaryDiskTotalCapacity':                      'float32',
+        'Census_PrimaryDiskTypeName':                           'category',
+        'Census_SystemVolumeTotalCapacity':                     'float32',
+        'Census_HasOpticalDiskDrive':                           'int8',
+        'Census_TotalPhysicalRAM':                              'float32',
+        'Census_ChassisTypeName':                               'category',
+        'Census_InternalPrimaryDiagonalDisplaySizeInInches':    'float16',
+        'Census_InternalPrimaryDisplayResolutionHorizontal':    'float16',
+        'Census_InternalPrimaryDisplayResolutionVertical':      'float16',
+        'Census_PowerPlatformRoleName':                         'category',
+        'Census_InternalBatteryType':                           'category',
+        'Census_InternalBatteryNumberOfCharges':                'float32',
+        'Census_OSVersion':                                     'category',
+        'Census_OSArchitecture':                                'category',
+        'Census_OSBranch':                                      'category',
+        'Census_OSBuildNumber':                                 'int16',
+        'Census_OSBuildRevision':                               'int32',
+        'Census_OSEdition':                                     'category',
+        'Census_OSSkuName':                                     'category',
+        'Census_OSInstallTypeName':                             'category',
+        'Census_OSInstallLanguageIdentifier':                   'float16',
+        'Census_OSUILocaleIdentifier':                          'int16',
+        'Census_OSWUAutoUpdateOptionsName':                     'category',
+        'Census_IsPortableOperatingSystem':                     'int8',
+        'Census_GenuineStateName':                              'category',
+        'Census_ActivationChannel':                             'category',
+        'Census_IsFlightingInternal':                           'float16',
+        'Census_IsFlightsDisabled':                             'float16',
+        'Census_FlightRing':                                    'category',
+        'Census_ThresholdOptIn':                                'float16',
+        'Census_FirmwareManufacturerIdentifier':                'float16',
+        'Census_FirmwareVersionIdentifier':                     'float32',
+        'Census_IsSecureBootEnabled':                           'int8',
+        'Census_IsWIMBootEnabled':                              'float16',
+        'Census_IsVirtualDevice':                               'float16',
+        'Census_IsTouchEnabled':                                'int8',
+        'Census_IsPenCapable':                                  'int8',
+        'Census_IsAlwaysOnAlwaysConnectedCapable':              'float16',
+        'Wdft_IsGamer':                                         'float16',
+        'Wdft_RegionIdentifier':                                'float16',
+        'HasDetections':                                        'int8'
+        }
+
+csv_read_start=datetime.datetime.now()
+df=pd.read_csv("train.csv", encoding="utf-8", dtype=dtypes)
+csv_read_end=datetime.datetime.now()
+
+etl_start=datetime.datetime.now()
+
+df.drop('MachineIdentifier', axis=1, inplace=True)
+
+
+#La magia... Convertir los strings en la cantidad de veces que aparece en el dataset.
+#Cuantos más registros únicos tenga, peor es hacer esto. En este caso, lo veo muy bien.
+for c in df.columns:
+    if ((df[c].dtype != "int8") & (df[c].dtype != "int16") & (df[c].dtype != "int32") & (df[c].dtype != "float16") & (df[c].dtype != "float32")):
+        df[c] = df[c].map(df[c].value_counts())
+
+
+etl_end=datetime.datetime.now()
+
+pickle_write_start=datetime.datetime.now()
+df.to_pickle("train_clean.pkl")
+pickle_write_end=datetime.datetime.now()
+
+
+print("input CSV read time (secs):", str((csv_read_end-csv_read_start).total_seconds()))
+print("ETL processing time (secs):", str((etl_end-etl_start).total_seconds()))
+print("output pickle read time (secs):", str((pickle_write_end-pickle_write_start).total_seconds()))
